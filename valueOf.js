51601d31626309046c3c56f3a853e82b5bfda04a
abahrisd <abakhri.sd@gmail.com>
2019-04-13 15:04:26 +0300

    valueOf fokus-pokus

---
+++ valueOf.js
@@ -0,0 +1,12 @@
+var a = {};
+
+a.valueOf = function(){
+    if (!this.i) {
+        this.i = 0;
+    }
+    this.i++;
+    return this.i;
+};
+
+console.log('a == a', a == a);
+console.log('a < a', a < a);
